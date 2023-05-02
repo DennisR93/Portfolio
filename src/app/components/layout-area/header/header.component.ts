65619658f11ed75ae3986d65fb661c5f7468e13d
DennisR93 <rdennis0993@gmail.com>
2023-05-02 10:57:59 +0300

    create mobile navbar

--- src/app/components/layout-area/header/header.component.ts
+++ src/app/components/layout-area/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  showToggle: boolean;
+
+  onToggle() {
+    this.showToggle = !this.showToggle;
+}
 
 }
