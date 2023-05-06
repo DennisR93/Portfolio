db01db5c7df47710febb9a4b6a2cdbb1c503998b
DennisR93 <rdennis0993@gmail.com>
2023-05-06 17:54:07 +0300

    add tablet size support

--- src/app/services/responsive.directive.ts
+++ src/app/services/responsive.directive.ts
@@ -8,7 +8,7 @@ export class ResponsiveDirective {
     constructor(private elementRef: ElementRef, private breakpointObserver: BreakpointObserver) {
 
         this.breakpointObserver
-            .observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape])
+            .observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape, Breakpoints.TabletPortrait])
             .subscribe({
                 next: (result: any) => {
                     console.log(result);
@@ -16,8 +16,16 @@ export class ResponsiveDirective {
                         if (result.breakpoints[breakpoint]) {
                             if (breakpoint === Breakpoints.HandsetPortrait) {
                                 this.elementRef.nativeElement.classList.remove("desktop");
+                                this.elementRef.nativeElement.classList.remove("tablet");
                             }
-                            if (breakpoint === Breakpoints.WebLandscape) { this.elementRef.nativeElement.classList.add("desktop"); }
+                            if(breakpoint === Breakpoints.TabletPortrait){
+                                this.elementRef.nativeElement.classList.add("tablet");
+                                this.elementRef.nativeElement.classList.remove("desktop");
+                            }
+                            if (breakpoint === Breakpoints.WebLandscape)
+                            { 
+                                this.elementRef.nativeElement.classList.add("desktop");
+                            }
                         }
                     }
                 },
