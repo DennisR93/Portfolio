1fac0ca282e21041e1f15677d433f5f0cac83a1f
DennisR93 <rdennis0993@gmail.com>
2023-05-06 17:53:00 +0300

    remove sticky header, add back to top

--- src/app/components/layout-area/layout/layout.component.ts
+++ src/app/components/layout-area/layout/layout.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.css']
 })
-export class LayoutComponent{
+export class LayoutComponent implements OnInit{
+
+  windowScrolled = false;
+
+  scrollToTop(): void {
+    window.scrollTo(0, 0);
+    };
+
+    ngOnInit(): void {
+      window.addEventListener("scroll", ()=> {
+        this.windowScrolled = window.scrollY !== 0;
+      });
+    }
+  }
 
 
-}
