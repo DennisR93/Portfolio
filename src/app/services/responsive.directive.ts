8f01319e3de7b872b1730e9a42dd332829fe8223
DennisR93 <rdennis0993@gmail.com>
2023-05-08 21:32:48 +0300

    add medium screen support

--- src/app/services/responsive.directive.ts
+++ src/app/services/responsive.directive.ts
@@ -8,22 +8,30 @@ export class ResponsiveDirective {
     constructor(private elementRef: ElementRef, private breakpointObserver: BreakpointObserver) {
 
         this.breakpointObserver
-            .observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape, Breakpoints.TabletPortrait])
+            // Observe Breakpoints
+            .observe([Breakpoints.HandsetPortrait, Breakpoints.WebLandscape, Breakpoints.TabletPortrait, Breakpoints.TabletLandscape])
             .subscribe({
                 next: (result: any) => {
-                    console.log(result);
                     for (let breakpoint of Object.keys(result.breakpoints)) {
                         if (result.breakpoints[breakpoint]) {
+                            // HandsetPortrait = Max width of 599px
                             if (breakpoint === Breakpoints.HandsetPortrait) {
                                 this.elementRef.nativeElement.classList.remove("desktop");
                                 this.elementRef.nativeElement.classList.remove("tablet");
                             }
+                            // Tablet Portrait = Min Width 600px, Max Width 839px
                             if(breakpoint === Breakpoints.TabletPortrait){
                                 this.elementRef.nativeElement.classList.add("tablet");
                                 this.elementRef.nativeElement.classList.remove("desktop");
                             }
+                            // Tablet Landscape = Min Width 960px, Max Width 1279px
+                            if(breakpoint === Breakpoints.TabletLandscape){
+                                this.elementRef.nativeElement.classList.add("tablet");
+                                this.elementRef.nativeElement.classList.remove("desktop");
+                            }
+                            // Desktop Landscape = Min Width 1280px
                             if (breakpoint === Breakpoints.WebLandscape)
-                            { 
+                            {
                                 this.elementRef.nativeElement.classList.add("desktop");
                                 this.elementRef.nativeElement.classList.remove("tablet");
                             }
@@ -33,5 +41,3 @@ export class ResponsiveDirective {
             });
     }
 }
-
-// Can be added parentElement before classList
